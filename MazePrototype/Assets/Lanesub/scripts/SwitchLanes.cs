6d3f35238208cf2d748a8ae9f1a5e5a3d04fc1a6
Logan M <mulderl@sheridancollege.ca>
2023-11-12 12:33:41 -0500

    lanegameadd
    added lane game

---
+++ MazePrototype/Assets/Lanesub/scripts/SwitchLanes.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class SwitchLanes : MonoBehaviour
+{
+    public float horizontalpos;
+    public float newhorizontalpos;
+    public float verticalpos = -6f;
+    public float newverticalpos;
+    public float speed = 10;
+    public float wiggleDuration = 0.5f;
+    public float wiggleSpeed = 30f;
+    private bool hasMoved;
+    private float hasMovedtimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        verticalpos = -6f;
+        horizontalpos = 0f;
+        newverticalpos = -6f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("a") && hasMoved == false)
+        {
+            StartCoroutine(WiggleCoroutine());
+            newhorizontalpos = Mathf.Clamp(horizontalpos - 2.4f, -2.4f, 2.4f);
+            hasMoved = true;
+            hasMovedtimer = 0;
+        }
+        if (Input.GetKeyDown("d") && hasMoved == false)
+        {
+            StartCoroutine(WiggleCoroutine());
+            newhorizontalpos = Mathf.Clamp(horizontalpos + 2.4f, -2.4f, 2.4f);
+            hasMoved = true;
+            hasMovedtimer = 0;
+        }
+        if (Input.GetKeyDown("s") && hasMoved == false)
+        {
+            StartCoroutine(WiggleCoroutine());
+            newverticalpos = Mathf.Clamp(verticalpos - 2f, -6f, -4f);
+            hasMoved = true;
+            hasMovedtimer = 0;
+        }
+        if (Input.GetKeyDown("w") && hasMoved == false)
+        {
+            StartCoroutine(WiggleCoroutine());
+            newverticalpos = Mathf.Clamp(verticalpos + 2f, -6f, -4f);
+            hasMoved = true;
+            hasMovedtimer = 0;
+        }
+        Move();
+        hasMovedtimer += Time.deltaTime;
+        if(hasMovedtimer >= 0.1f)
+        {
+            hasMoved = false;
+        }
+        
+        
+        
+
+    }
+    void Move()
+    {
+        
+        transform.position = Vector3.MoveTowards(transform.position, new Vector3(newhorizontalpos, 0.5f, newverticalpos), speed * Time.deltaTime);
+        horizontalpos = newhorizontalpos;
+        verticalpos = newverticalpos;
+    }
+    IEnumerator WiggleCoroutine()
+    {
+        float elapsedTime = 0f;
+
+        while (elapsedTime < wiggleDuration)
+        {
+            // Calculate the new scale based on a sine wave for a wiggling effect
+            float yOffset = Mathf.Sin(Time.time * wiggleSpeed) * ((wiggleDuration - elapsedTime) / wiggleDuration) * 0.2f; // Adjust the amplitude as needed
+                                                                                                                           
+            transform.localScale = new Vector3(yOffset*5 + 1, yOffset + 1, yOffset + 1);
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        // Reset the scale after the wiggle duration
+        transform.localScale = new Vector3(1, 1, 1);
+        
+    }
+}
+
+
+
