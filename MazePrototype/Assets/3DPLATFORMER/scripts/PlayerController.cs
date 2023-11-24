77d66129a61ff7c2c1d0627fba157e240e8968b3
Jonathan To <jonto398@gmail.com>
2023-11-24 00:27:54 -0500

    WIP clamped accel

--- MazePrototype/Assets/3DPLATFORMER/scripts/PlayerController.cs
+++ MazePrototype/Assets/3DPLATFORMER/scripts/PlayerController.cs
@@ -12,6 +12,7 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField] float accelSpeed;
     [SerializeField] float maxSpeed;
+    [SerializeField] float airControl;
     [SerializeField] float jumpPower;
     [SerializeField] LayerMask groundLayer;
 
@@ -29,11 +30,13 @@ public class PlayerController : MonoBehaviour
         Move();
         Jump();
         AnimChecks();
+
+        Debug.DrawRay(transform.position, Vector3.down* 1.2f);
     }
 
     bool isGrounded()
     {
-        if (Physics.Raycast(transform.position, Vector3.down, 2f, groundLayer))
+        if (Physics.Raycast(transform.position, Vector3.down, 1.2f, groundLayer))
         {
             return true;
         }
@@ -50,14 +53,23 @@ public class PlayerController : MonoBehaviour
 
     void Move()
     {
-        rb.AddForce(moveDir.normalized * accelSpeed, ForceMode.Acceleration);
-
-        if (rb.velocity.magnitude > maxSpeed)
+        if (isGrounded())
         {
-            rb.velocity = rb.velocity.normalized * maxSpeed;
+            rb.AddForce(moveDir.normalized * accelSpeed, ForceMode.Acceleration);
+        }
+        else
+        {
+            rb.AddForce(moveDir.normalized * accelSpeed * airControl, ForceMode.Acceleration);
         }
 
-        //rb.velocity = new Vector3(xInput * moveSpeed, rb.velocity.y, zInput * moveSpeed); 
+        Vector3 groundVel = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+        if (groundVel.magnitude > maxSpeed)
+        {
+            Vector3 clampedVel = groundVel.normalized * maxSpeed;
+            rb.velocity = new Vector3(clampedVel.x, rb.velocity.y, clampedVel.z);
+        }
+
+        //rb.velocity = new Vector3(xInput * moveSpeed, rb.velocity.y, zInput * moveSpeed); //Use GetAxis for acceleration, but breaks rb.addforce
     }
 
     void Jump()
@@ -68,6 +80,7 @@ public class PlayerController : MonoBehaviour
             rb.AddForce(transform.up * jumpPower, ForceMode.Impulse);
         }
     }
+
     void AnimChecks()
     {
         if (rb.velocity == Vector3.zero)
