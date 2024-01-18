359f5a47358255505d7dcd45e2917d758a1d3f77
Jonathan To <jonto398@gmail.com>
2024-01-17 23:28:48 -0500

    Cleaned up swinging code

--- MazePrototype/Assets/3DPLATFORMER/Scripts/Swinging.cs
+++ MazePrototype/Assets/3DPLATFORMER/Scripts/Swinging.cs
@@ -6,35 +6,49 @@ using UnityEngine;
 public class Swinging : MonoBehaviour
 {
     //HingeJoint hinge;
-    //FixedJoint hinge;
-    CharacterJoint hinge;
+    FixedJoint hinge;
+    //CharacterJoint hinge;
 
+    GameObject rope;
 
     bool canSwing;
     bool isSwinging;
-    PlayerController player;
-    Rigidbody rb;
-    [SerializeField] Rigidbody ropeBottom;
     [SerializeField] float swingForce;
 
     private void Update()
     {
-
-    }
-
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.gameObject.tag == "Rope")
+        if(canSwing)
         {
             if (Input.GetKeyDown(KeyCode.E) && hinge == null)
             {
-                hinge = transform.AddComponent<CharacterJoint>();
-                hinge.connectedBody = other.transform.parent.GetComponent<Rigidbody>();
-            }
-            else if(Input.GetKeyDown(KeyCode.E) && hinge != null)
-            {
-                Destroy(hinge);
+                hinge = transform.AddComponent<FixedJoint>();
+                hinge.connectedBody = rope.transform.parent.GetComponent<Rigidbody>();
+                isSwinging = true;
+                canSwing = false;
             }
         }
+        else if (isSwinging)
+        {
+            if (Input.GetKeyDown(KeyCode.E) && hinge != null)
+            {
+                Destroy(hinge);
+                isSwinging = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Rope")
+        {
+            rope = other.gameObject;
+            canSwing = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        canSwing = false;
+        rope = null;
     }
 }
