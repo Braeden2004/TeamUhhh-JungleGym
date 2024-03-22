7f380bee10a8165fa1c54d6eb26067ce0bdd1260
Braeden Hammill <hammillb@sheridancollege.ca>
2024-03-22 17:53:18 -0400

    revert

--- JungleGymProject/Assets/3DPLATFORMER/Scripts/BreakableWall.cs
+++ JungleGymProject/Assets/3DPLATFORMER/Scripts/BreakableWall.cs
@@ -9,39 +9,11 @@ public class BreakableWall : MonoBehaviour
     public Rigidbody rb;
     public float minVelocity;
 
-    public int freezeTimeTotal;
-    private int currentfreezeTime;
-    public bool freeze;
-
 
     //getrollscript
     private void Start()
     {
         rollscript = GameObject.Find("PlayerPlaceHolder").GetComponent<Roll>();
-    }
-
-    private void Update()
-    {
-        if (freeze ==true)
-        {
-            //freeze the whole game for 2 frames
-            Time.timeScale = 0f;
-
-            //set the freeze time
-            currentfreezeTime = freezeTimeTotal;
-            currentfreezeTime--;
-
-            if (freezeTimeTotal <= 0)
-            {
-                Time.timeScale = 1f;
-
-                //destroy the wall
-                Destroy(gameObject);
-
-                freeze = false;
-            }
-        }
-        
     }
 
     //check for a trigger enter
@@ -57,7 +29,8 @@ public class BreakableWall : MonoBehaviour
             {
                 if (rb.velocity.magnitude > minVelocity)
                 {
-                    freeze = true;    
+                    //destroy the wall
+                    Destroy(gameObject);
                 }
             }
         }
