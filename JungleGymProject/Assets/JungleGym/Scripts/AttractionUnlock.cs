632bc1b86f69f48efe5b888e9c6d6d891030033d
Braeden2004 <hammillb@sheridancollege.ca>
2024-05-17 21:28:33 -0400

    fixed attraction spawning
    instead of seting active, i hide them underground until unlocked. this fixes the ticket bug that heppens when they are deactivated

--- JungleGymProject/Assets/JungleGym/Scripts/AttractionUnlock.cs
+++ JungleGymProject/Assets/JungleGym/Scripts/AttractionUnlock.cs
@@ -9,6 +9,9 @@ public class AttractionUnlock : MonoBehaviour
     public GameObject attraction;
     public GameObject cover;
 
+    public float offsetY;
+    public Vector3 attractionSpawnPos;
+
     //get script of button
     public buttonScript buttonScript;
 
@@ -16,13 +19,18 @@ public class AttractionUnlock : MonoBehaviour
     public ParticleSystem unlockParticle;
     public bool particlePlayed = false;
 
+    void Awake()
+    {
+        attractionSpawnPos = attraction.transform.position;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         //NOTE: The attraction starts active because it needs 1 frame at the beginning to tell the game how many clipboards are in the world, the update method will then turn it off after frame 1
-
-        attraction.SetActive(true); //make attraction dissapear
         cover.SetActive(true); //make cover appear
+
+        attraction.transform.position = new Vector3(transform.position.x, transform.position.y - offsetY, transform.position.z);
     }
 
     // Update is called once per frame
@@ -35,7 +43,13 @@ public class AttractionUnlock : MonoBehaviour
         {
             //change active states
             cover.SetActive(false); //make cover dissapear
-            attraction.SetActive(true); //make attraction appear
+
+            //teleport attraction above ground
+            if (attraction.transform.position != attractionSpawnPos)
+            {
+                attraction.transform.position = attractionSpawnPos;
+            }
+            
 
             //Spawn Effects
             if (particlePlayed == false)
@@ -46,7 +60,8 @@ public class AttractionUnlock : MonoBehaviour
         }
         else
         {
-            attraction.SetActive(false); //make attraction dissapear
+            //teleport attraction underground
+            
         }
 
         
