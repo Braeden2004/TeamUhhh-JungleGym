74c0083503f1d3a185cb2208a436b772ed18b20b
Braeden2004 <hammillb@sheridancollege.ca>
2024-07-04 23:44:50 -0400

    final world details

--- JungleGymProject/Assets/Other Files/LaneGameAssets/Scripts/destroyObject.cs
+++ JungleGymProject/Assets/Other Files/LaneGameAssets/Scripts/destroyObject.cs
@@ -6,26 +6,13 @@ public class destroyObject : MonoBehaviour
 {
     private AudioSource audioSource; // Reference to the AudioSource component
 
-    private void Start()
+    private void Update()
     {
-        audioSource = GetComponent<AudioSource>(); // Get the AudioSource component on this object
-    }
-
-    // Detect player touching the spawned object
-    private void OnTriggerEnter(Collider other)
-    {
-        Debug.Log("Player touched the object"); // Debug message (optional)
-
-        // Play the audio clip
-        if (audioSource != null && audioSource.clip != null)
+        //if player has all clipboards
+        if (ScoreManager.instance.clipboardTotal == ScoreManager.instance.totalClipboardInScene)
         {
-            audioSource.Play();
+            //destroy the object
+            Destroy(gameObject);
         }
-
-        //Teleport object to make it appear as if it despawned instantly
-        transform.position = new Vector3(0,-1000f,0);
-
-        // Destroy the object after the sound has finished playing
-        Destroy(gameObject, audioSource.clip.length);
     }
 }
