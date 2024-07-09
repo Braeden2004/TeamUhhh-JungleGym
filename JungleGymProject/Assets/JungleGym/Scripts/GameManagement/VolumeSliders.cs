5447c0e1ddfbbab841d91408c2d1ee330047c95c
Juan Alarcon <alarcoju@sheridancollege.ca>
2024-07-08 20:39:46 -0400

    Added volume sliders fix

--- JungleGymProject/Assets/JungleGym/Scripts/GameManagement/VolumeSliders.cs
+++ JungleGymProject/Assets/JungleGym/Scripts/GameManagement/VolumeSliders.cs
@@ -10,15 +10,27 @@ public class VolumeSliders : MonoBehaviour
     public void SetMusicVolume(float musicVol)
     {
         audioMixer.SetFloat("musicVol", musicVol);
+        if (musicVol == -20f)
+        {
+            audioMixer.SetFloat("musicVol", -80);
+        }
     }
 
     public void SetSFXVolume(float sfxVol)
     {
         audioMixer.SetFloat("sfxVol", sfxVol);
+        if (sfxVol == -20f)
+        {
+            audioMixer.SetFloat("musicVol", -80);
+        }
     }
 
     public void SetAmbienceVolume(float ambienceVol)
     {
         audioMixer.SetFloat("ambienceVol", ambienceVol);
+        if (ambienceVol == -20f)
+        {
+            audioMixer.SetFloat("musicVol", -80);
+        }
     }
 }
